34ef477ff3c1a9e935de705d8dada03e644da93c
baonh <nguyenhuybao11082003@gmail.com>
2026-04-07 17:30:42 +0700

    est SmolVLM instead of Qwen2-VL-7B

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import torch
 from transformers import Qwen2VLForConditionalGeneration, AutoProcessor
 from peft import LoraConfig, get_peft_model
 
-MODEL_ID = "Qwen/Qwen2-VL-7B-Instruct"
+MODEL_ID = "HuggingFaceTB/SmolVLM-Instruct"
 
 
 def load_model_and_processor():
